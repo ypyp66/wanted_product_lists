92f7193c993d85623309672204542f75d8099edd
mukjo96 <mukjo96@gmail.com>
2021-07-30 02:39:50 +0900

    feat: Product 페이지 기본 작업
    params 변화 감지하여 state 변경

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,55 +4,55 @@ import CardList from "components/CardList/index";
 import PageTitle from "components/Text/pageTitle";
 import Button from "../components/Button/index";
 import LinkButton from "../components/LinkButton/index";
+import { getJsonData } from "utils/getJsonData";
 
 const HomeContainer = styled.div`
-  max-width: 1080px;
-  margin: auto;
+    max-width: 1080px;
+    margin: auto;
 `;
 
 const HeaderContainer = styled.div`
-  margin-left: 24px;
-  margin-top: 24px;
+    margin-left: 24px;
+    margin-top: 24px;
 `;
 
 const RandomProductButton = styled(Button)`
-  width: 100px;
-  background-color: pink;
-  margin-top: 24px;
+    width: 100px;
+    background-color: pink;
+    margin-top: 24px;
 `;
 
 class Home extends Component {
-  state = {
-    products: [],
-  };
+    state = {
+        products: [],
+    };
 
-  async componentDidMount() {
-    const response = await fetch("http://localhost:3000/data/productList.json");
-    const products = await response.json();
-    // TODO: not-intertest 는 필터링한 다음에 set을 해줘야할까?
-    // TODO: 어떻게 커스터마이징
-    this.setState({
-      products,
-    });
-  }
+    async componentDidMount() {
+        const products = await getJsonData();
+        // TODO: not-intertest 는 필터링한 다음에 set을 해줘야할까?
+        // TODO: 어떻게 커스터마이징
+        this.setState({
+            products,
+        });
+    }
 
-  render() {
-    return (
-      <HomeContainer>
-        <HeaderContainer>
-          <PageTitle title="상품 목록" />
-          <RandomProductButton
-            onClick={() => console.log("랜덤상품조회 클릭!")}
-          >
-            랜덤 상품 조회
-          </RandomProductButton>
-        </HeaderContainer>
+    render() {
+        return (
+            <HomeContainer>
+                <HeaderContainer>
+                    <PageTitle title="상품 목록" />
+                    <RandomProductButton
+                        onClick={() => console.log("랜덤상품조회 클릭!")}
+                    >
+                        랜덤 상품 조회
+                    </RandomProductButton>
+                </HeaderContainer>
 
-        <CardList cards={this.state.products} />
-        <LinkButton title="조회 이력" to="/recentList" />
-      </HomeContainer>
-    );
-  }
+                <CardList cards={this.state.products} />
+                <LinkButton title="조회 이력" to="/recentList" />
+            </HomeContainer>
+        );
+    }
 }
 
 Home.propTypes = {};
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,24 +1,110 @@
-import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
+import PageTitle from "components/Text/pageTitle";
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import styled from "styled-components";
+import { getJsonData } from "utils/getJsonData";
 
 class Product extends Component {
-  constructor(props) {
-    super(props);
-    const { match } = this.props;
-    console.log(match.params.id);
-  }
+    state = {
+        products: [],
+        id: null,
+    };
 
-  componentDidMount() {
-    // const { match } = this.props;
-    // console.log(match.params.id);
-  }
+    async componentDidMount() {
+        const products = await getJsonData();
+        const { id } = this.props.match.params;
+        this.setState({
+            products,
+            id,
+        });
+    }
 
-  render() {
-    return <div>product</div>;
-  }
+    async componentDidUpdate(prevProps) {
+        if (this.props.match.params !== prevProps.match.params) {
+            const { id } = this.props.match.params;
+            this.setState({
+                id,
+            });
+        }
+    }
+
+    render() {
+        if (!this.state.products[this.state.id]) {
+            return <div>Loading</div>;
+        } else {
+            const { title, brand, price } = this.state.products[this.state.id];
+            return (
+                <ProductContainer>
+                    <HeaderContainer>
+                        <PageTitle title="상품 상세 페이지" />
+                    </HeaderContainer>
+                    <ItemContainer>
+                        <Title>{title}</Title>
+                        <Brand>{brand}</Brand>
+                        <Price>
+                            <strong>{price}</strong>원
+                        </Price>
+                    </ItemContainer>
+                </ProductContainer>
+            );
+        }
+    }
 }
 
 Product.propTypes = {};
 
 // NOTE: withRouter 를 써야하는이유는?
 export default withRouter(Product);
+
+const ProductContainer = styled.div`
+    max-width: 1080px;
+    min-height: 100vh - 24px;
+    margin: auto;
+`;
+
+const HeaderContainer = styled.div`
+    margin-left: 24px;
+    margin-top: 24px;
+`;
+
+const ItemContainer = styled.div`
+    width: 50%;
+    padding: 5%;
+    box-shadow: 0 1px 3px rgba(0, 0, 0, 0.12), 0 1px 2px rgba(0, 0, 0, 0.24);
+    border-radius: 5%;
+    h2,
+    h3,
+    h4 {
+        margin-bottom: 5%;
+    }
+    margin: auto;
+`;
+
+const Title = styled.h2`
+    overflow: hidden;
+    font-weight: 600;
+    text-overflow: ellipsis;
+    font-size: 26px;
+    line-height: 36px;
+    height: 72px;
+    max-height: 72px;
+
+    :hover {
+        color: darkblue;
+    }
+    /* -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical; */
+`;
+
+const Brand = styled.h4`
+    font-size: 16px;
+    color: #2d2d2d;
+`;
+
+const Price = styled.h3`
+    font-size: 18px;
+    strong {
+        font-weight: 600;
+        font-size: 20px;
+    }
+`;
