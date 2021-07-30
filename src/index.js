078751927e34686132015498ab66f5c6407cb8f8
kitebeen <ypyp66@naver.com>
2021-07-31 00:39:43 +0900

    경로변경

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import GlobalStyles from "GlobalStyles.js";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import GlobalStyles from './GlobalStyles.js';
 
 ReactDOM.render(
   <React.StrictMode>
     <GlobalStyles />
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root'),
 );
