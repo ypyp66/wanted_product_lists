25230d88a56b9113a29e2dc4c23649c4d78dceae
kitebeen <ypyp66@naver.com>
2021-07-31 00:26:50 +0900

    fix: url 주소 변경

--- src/utils/getJsonData.js
+++ src/utils/getJsonData.js
@@ -1,8 +1,8 @@
-const JSON_URL = "http://localhost:3000/data/productList.json";
+const JSON_URL = '/data/productList.json';
 
 export async function getJsonData() {
-    const response = await fetch(JSON_URL);
-    const products = await response.json();
+  const response = await fetch(JSON_URL);
+  const products = await response.json();
 
-    return products;
+  return products;
 }
